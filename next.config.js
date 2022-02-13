e7d5250d931f6c2b7cce351a3a97cac4c163a7d2
wdrik <w.drik@outlook.com>
2022-02-13 18:55:02 -0300

    chore: add loader propert

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  target: "serverless",
+  loader: "imgix",
   images: {
     domains: ["links.papareact.com", "image.tmdb.org"],
   },
